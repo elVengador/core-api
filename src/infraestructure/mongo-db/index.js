6838d99c6a32c2aa89737c94b4ee94ac5fc9c84f
elVengador <jimynicanor1001@gmail.com>
2022-02-07 15:14:03 -0500

    updatemonogdb conection

--- src/infraestructure/mongo-db/index.js
+++ src/infraestructure/mongo-db/index.js
@@ -7,8 +7,8 @@ let dbClient = null
 const main = async () => {
     try {
         const uri = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}?retryWrites=true&w=majority`
-        // dbClient = new MongoClient(uri);
-        dbClient = new MongoClient(uri, { useUnifiedTopology: true });
+        dbClient = new MongoClient(uri);
+        // dbClient = new MongoClient(uri, { useUnifiedTopology: true });
         await dbClient.connect();
         console.log("[💽 MongoDB]: connected successfull");
     } catch (err) {
