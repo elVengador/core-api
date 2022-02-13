e84ceee3061d102d42be184531a8909fe8b4120c
elVengador <jimynicanor1001@gmail.com>
2022-02-13 13:35:37 -0500

    delete unused core files

--- src/infraestructure/graphql/core/user.resolver.js
+++
@@ -1,9 +0,0 @@
-import * as userController from '../../../application/controllers/user.controller';
-
-const userResolver = {
-    getUser: userController.getUser,
-    // getUsers: userController.getUsers,
-    addUser: userController.addUser
-}
-
-export { userResolver }
