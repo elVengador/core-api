6e72a4a9912d76b9e5faf652c9f5f32f58246210
elVengador <dev.jimynicanor@gmail.com>
2023-12-09 13:45:45 -0500

    update default env variables

--- src/core.enviroments.js
+++ src/core.enviroments.js
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const dbHost = process.env.DB_MONGODB_HOST
-export const dbPort = process.env.DB_MONGODB_PORT
-export const dbUser = process.env.DB_MONGODB_USER
-export const dbPass = process.env.DB_MONGODB_PASS
-export const dbName = process.env.DB_MONGODB_NAME
-export const dbRedisHost = process.env.DB_REDIS_HOST
-export const dbRedisPort = process.env.DB_REDIS_PORT
-export const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
-export const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET
+export const dbHost = process.env.DB_MONGODB_HOST||"127.0.0.1"
+export const dbPort = process.env.DB_MONGODB_PORT||"5062"
+export const dbUser = process.env.DB_MONGODB_USER||"dev"
+export const dbPass = process.env.DB_MONGODB_PASS||"123123123"
+export const dbName = process.env.DB_MONGODB_NAME||"dev-database"
+export const dbRedisHost = process.env.DB_REDIS_HOST||"127.0.0.1"
+export const dbRedisPort = process.env.DB_REDIS_PORT||"5063"
+export const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET||"shhhh-access-12312"
+export const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET||"shhhh-refresh-14552"
