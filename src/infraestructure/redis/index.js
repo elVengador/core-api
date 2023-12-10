72f4ce91752127c3094af8e96851b95fce907aa9
elVengador <dev.jimynicanor@gmail.com>
2023-12-09 21:45:24 -0500

    update redis connection

--- src/infraestructure/redis/index.js
+++ src/infraestructure/redis/index.js
@@ -1,10 +1,10 @@
 import { createClient } from 'redis'
-import { dbRedisHost, dbRedisPort } from '../../core.enviroments';
+import { dbRedisHost, dbRedisPort,dbRedisPassword } from '../../core.enviroments';
 
 // redis[s]://[[username][:password]@][host][:port][/db-number]
-const client = createClient({
-    url: `redis://${dbRedisHost}:${dbRedisPort}`
-});
+const url = `redis://:${dbRedisPassword}@${dbRedisHost}:${dbRedisPort}`
+console.log({url})
+const client = createClient({url});
 
 const main = async () => {
     await client.connect()
